12953eae8837d617102c0b12687027db8f4cba42
cloudz1 <55038721+cloudz1@users.noreply.github.com>
2019-09-07 22:18:24 -0400

    Create allports.sh
    Script uses hping3 to quickly scan all 65535 ports then sends the discovered ports to nmap for further recon.  Creates two files, portsout.txt for hping output and portscans for nmap.  A separate UDP scan is required as this scan is TCP only since full UDP scans are quick.

---
+++ allports.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Enter an IP address (eg 10.10.10.10)";
+read userinput
+hping3 --scan 1-65535 -S $userinput > test.txt;
+cat test.txt | grep -v "(" > portsout.txt;
+rm test.txt;
+list=$( cat portsout.txt | awk '{ print $1 }' | tr '\n' ',' | sed 's/.$//' )
+nmap -sV $list $userinput -oA portscans;
+echo "===================================================="
+echo "Scan complete!"
