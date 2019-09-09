c42adb3c3a76d5f213c3b7b91b497666d42c8984
cloudz1 <55038721+cloudz1@users.noreply.github.com>
2019-09-08 23:52:11 -0400

    Update allports.sh

--- allports.sh
+++ allports.sh
@@ -5,6 +5,6 @@ hping3 --scan 1-65535 -S $userinput > test.txt;
 cat test.txt | grep -v "(" > portsout.txt;
 rm test.txt;
 list=$( cat portsout.txt | awk '{ print $1 }' | tr '\n' ',' | sed 's/.$//' )
-nmap -sV $list $userinput -oA portscans;
+nmap -Pn -sV $list $userinput -oA portscans;
 echo "===================================================="
 echo "Scan complete!"
