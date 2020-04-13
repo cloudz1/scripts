72048fac4cf5158aec93cfd47a8e6a5df9da38e5
cloudz1 <55038721+cloudz1@users.noreply.github.com>
2020-04-12 23:00:05 -0400

    Update allports.sh

--- allports.sh
+++ allports.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+ap='_allports'
+depth='_scan'
 echo "Enter an IP address (eg 10.10.10.10)";
 read userinput
-hping3 --scan 1-65535 -S $userinput > test.txt;
-cat test.txt | grep -v "(" > portsout.txt;
-rm test.txt;
-list=$( cat portsout.txt | awk '{ print $1 }' | tr '\n' ',' | sed 's/.$//' )
-nmap -Pn -sV $list $userinput -oA portscans;
+hping3 --scan 1-65535 -S $userinput > placeholder.txt;
+cat placeholder.txt | grep -v "(" > $userinput$ap.txt;
+rm placeholder.txt;
+list=$( cat $userinput$ap.txt | awk '{ print $1 }' | tr '\n' ',' | sed 's/.$//' )
+nmap -sC -Pn -sV $list $userinput -oA $userinput$depth;
 echo "===================================================="
 echo "Scan complete!"
